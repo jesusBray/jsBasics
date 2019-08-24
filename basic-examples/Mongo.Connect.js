24af3ab32e0aad1188599be7008f97e81c6c3467
Jesus Br P <jesus.brayan.paye@gmail.com>
2019-08-24 18:02:06 -0400

    example in mongo access

---
+++ basic-examples/Mongo.Connect.js
@@ -0,0 +1,38 @@
+
+
+// let Start = ()=>{
+//     console.log("buen dia");
+// }
+
+const Start='esta es la funcion estar';
+// var Connet;
+
+function Connect() {
+    return Start;
+}
+
+// let Mongo = {
+//     connect:Connect,
+//     name:'jesus'
+
+// };
+
+// Mongo.Connect().Start();
+
+let Mongo= {
+    connet:Connect,
+};
+
+Mongo.connet().Start;
+
+
+
+
+let a = {
+    dato:'aa',
+    dato2:'bb',
+    dato3:'cc',
+    func:function(){
+        
+    }
+};
