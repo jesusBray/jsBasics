8ea12c5642ae9cbbea9129611e020b4fb7a53da7
Jesus Br P <jesus.brayan.paye@gmail.com>
2019-08-31 00:11:01 -0400

    example1

---
+++ basic-examples2/example1.js
@@ -0,0 +1,53 @@
+
+
+console.log((function(){
+    // let year = 1900
+    //     let total=0;
+        
+        // if(year > 0 && year >= 100 ){
+        //     if(year % 100 == 0){
+        //         return year/100;
+        //     }
+            
+    //                      ejercicio1;
+                // total = year%100;
+                // if(total>0){
+                //     let a = year - total;
+                //     return a/100+1;
+                // }
+                // let a = year - total;
+                // return year/100;
+                // ejercicio 2
+                // let palindrome="hola",cadena="";
+                // for (let index = palindrome.length-1; index >= 0 ; index--) {                    
+                //     cadena+=palindrome[index];   
+                // }
+                // if(cadena == palindrome){
+                //     return true;
+                // }
+                // else{
+                //     return false;
+                // }
+                //ejercico3
+
+                // let vec = [-23, 4, -3, 8, -12];
+                // let numb= -1000000000;
+                // for (let i = 0; i < vec.length; i++) {
+                //     let resul = vec[i]*vec[i+1];
+                //     if(resul>numb){
+                //         numb=resul;
+                //     }
+                // }
+                // console.log(numb);
+                //examsple 4
+                
+                
+
+
+            
+        // }
+        // else{
+        //     return 0;
+        // }
+    
+})());
